50182209741707550d66d1a42ce719941e148876
Rashu3236 <rashuhansi@gmail.com>
2026-04-11 11:30:00 +0530

    Add axios API client for wardrobe endpoints

---
+++ frontend/src/api/config.js
@@ -0,0 +1,4 @@
+// Run `ipconfig` (Windows) or `ifconfig` (Mac/Linux) and use your IPv4 address.
+const BASE_URL = 'http://10.123.244.242:8000';
+
+export default BASE_URL;
---
+++ frontend/src/api/wardrobeApi.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import BASE_URL from './config';
+
+const api = axios.create({ baseURL: BASE_URL, timeout: 10000 });
+
+export const fetchOrganizedItems = async () => {
+    const res = await api.get('/items/organized');
+    return res.data;
+};
+
+export const fetchInsights = async () => {
+    const res = await api.get('/items/insights');
+    return res.data; // { dirty_count, underused: [ids], overused: [ids] }
+};
+
+export const wearItem = async (itemIdStr) => {
+    const res = await api.post(`/items/wear/${itemIdStr}`);
+    return res.data;
+};
+
+export const washItem = async (itemIdStr) => {
+    const res = await api.post(`/items/wash/${itemIdStr}`);
+    return res.data;
+};
